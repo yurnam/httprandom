15be3e60245a06b578c97e3ff43fa5a5d9c01f57
martin <m.kritzer@gmx.at>
2025-12-14 11:39:10 +0100

    add files

---
+++ llmintegrator.py
@@ -0,0 +1,13 @@
+from ollama import Client
+
+class LLMIntegrator:
+    def __init__(self):
+        self.client = Client(host = "http://192.168.19.187:11434")
+        self.model_name = "granite4:latest"
+
+    def generate_response(self, prompt):
+        messages = {"role": "user", "content": prompt}
+        response = self.client.chat(model=self.model_name, messages=[messages])
+        return response ['message'] ['content']
+
+

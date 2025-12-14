19ef08349f8057b64593c0cdd39a19dfe6738b2d
martin <m.kritzer@gmx.at>
2025-12-14 14:14:10 +0100

    gemma3:1b

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ ai_responses_lock = threading.Lock()  # Protect shared dictionary from race cond
 app.config["MAX_CONTENT_LENGTH"] = 256 * 1024  # 256 KiB
 
 llm = llmintegrator.LLMIntegrator()
-llm.model_name = "moondream:latest"
+llm.model_name = "gemma3:1b"
 
 REMOVE_PATTERNS = [
     "```html", "```HTML", "```",  # common fences

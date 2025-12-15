fdd4ee491b110a01173e46c0e56e3110ac994728
martin <m.kritzer@gmx.at>
2025-12-15 12:34:55 +0100

    gemma3:1b

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ ai_responses_lock = threading.Lock()  # Protect shared dictionary from race cond
 app.config["MAX_CONTENT_LENGTH"] = 256 * 1024  # 256 KiB
 
 llm = llmintegrator.LLMIntegrator()
-llm.model_name = "llama3.1:8b"
-llm.model_name = "granite4:350m"
+llm.model_name = "deepseek-r1:1.5b"
 
 REMOVE_PATTERNS = [
     "```html", "```HTML", "```",  # common fences
